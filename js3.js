abbaea93d43e683fd18650ae9753519af6707695
agrawalvs <103302374+agrawalvs@users.noreply.github.com>
2022-06-22 23:55:43 +0530

    prac6

--- js3.js
+++
@@ -1,9 +0,0 @@
-var a = window.prompt("Enter your Marks");
-if (a > 90)
-    alert("Your Grade is A+");
-else if (a >= 80 & a <= 90)
-    alert("Your Grade is A");
-else if (a >= 66 & a < 80)
-    alert("Your Grade is B");
-else if (a >= 50 & a <= 65)
-    alert("Your Grade is C");    
